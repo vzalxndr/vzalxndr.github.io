8187a3244be8a7c7abe78187f588d9414f3157a3
vzalxndr <vzalxndr.reserve@gmail.com>
2025-04-02 23:24:35 +0200

    menu + favicons added

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const closeBtn = document.getElementById("close-btn");
+    const body = document.body;
+    
+    menuBtn.addEventListener("click", () => {
+        body.classList.add("show-menu");
+    });
+    
+    closeBtn.addEventListener("click", () => {
+        body.classList.remove("show-menu");
+    });
+});
