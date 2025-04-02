24ce97479d5213d581d17f23ef4ea6a9609ec469
vzalxndr <vzalxndr.reserve@gmail.com>
2025-04-02 23:47:09 +0200

    styles

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
-const $menuBtn = document.getElementById("menu-btn");
-const $closeBtn = document.getElementById("close-btn");
-const $body = document.body;
-
-$menuBtn.addEventListener("click", () => {
-    $body.classList.add("show-menu");
-});
-
-$closeBtn.addEventListener("click", () => {
-    $body.classList.remove("show-menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const $menuBtn = document.getElementById("menu-btn");
+    const $closeBtn = document.getElementById("close-btn");
+    const $body = document.body;
+    
+    $menuBtn.addEventListener("click", () => {
+        $body.classList.add("show-menu");
+    });
+    
+    $closeBtn.addEventListener("click", () => {
+        $body.classList.remove("show-menu");
+    });
 });
